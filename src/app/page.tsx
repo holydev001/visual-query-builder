3a4f85d5db35bed9d77d3a6abcf687fe311b776e
David Adams <davebenaaa@gmail.com>
2026-06-03 02:53:46 +0100

    feat(validation): add recursive query validation engine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,14 @@ import History from "../../components/query-controls/History";
 
 import KeyboardShortcuts from "../../components/query-controls/KeyboardShortcuts";
 
+import ValidationPanel from "@/components/query-controls/ValidationPanel";
+
 export default function Home() {
   return (
     <div className="p-8 space-y-10">
       <QueryControls />
+
+      <ValidationPanel />
 
       <History />
 
